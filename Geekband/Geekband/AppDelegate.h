afb7905750d61ecea80a0393c71e6ade321a19af
mac <sleepinge@sleepinematombp.corp.carestreamhealth.com>
2015-11-26 14:01:51 +0800

    commit

---
+++ Geekband/Geekband/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geekband
+//
+//  Created by sleepinge on 15/11/26.
+//  Copyright © 2015年 sleepinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Geekband/Geekband/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Geekband
+//
+//  Created by sleepinge on 15/11/26.
+//  Copyright © 2015年 sleepinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
